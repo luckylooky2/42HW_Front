ae737122f1e93993db6e58151e0833ddd423fb12
Chanhyung Lee <luckylooky2@naver.com>
2023-08-24 19:54:04 +0900

    comment : request in AuthProvider comment off

--- src/utils/AuthProvider.tsx
+++ src/utils/AuthProvider.tsx
@@ -18,7 +18,6 @@ export const AuthContext = createContext<{
 });
 
 const AuthProvider: FC<Props> = ({ children }) => {
-  // const navigate = useNavigate();
   // useState를 사용하면, context를 자식 컴포넌트에서도 업데이트할 수 있음
   const [myInfo, setMyInfo] = useState<IUser | null>(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -26,19 +25,6 @@ const AuthProvider: FC<Props> = ({ children }) => {
     () => ({ myInfo, setMyInfo, isLoading, setIsLoading }),
     [myInfo, setMyInfo, isLoading, setIsLoading]
   );
-
-  // useEffect(() => {
-  //   // 토큰을 가지고 나의 정보 요청
-  //   // 성공 ? context에 나의 정보 저장 : status code 401 => 로그인 화면으로 이동
-  //   try {
-  //     const result = axios.get(`${API_URL}/user/me`);
-  //     setNickname("chanhyle");
-  //   } catch (e) {
-  //     // redirect를 하면 무한 라우팅 발생 => 클라이언트 라우팅이 필요
-  //     alert("로그인 정보가 유효하지 않습니다. 다시 로그인 해주세요.");
-  //     navigate("/login");
-  //   }
-  // }, []);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
