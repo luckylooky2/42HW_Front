f3f92d93fdf8c2caf12d727754c27150927081ff
luckylooky2 <luckylooky2@naver.com>
2023-08-26 09:04:11 +0900

    fix : unnecessary character

--- src/components/Main/ChatButton.tsx
+++ src/components/Main/ChatButton.tsx
@@ -10,7 +10,7 @@ const ChatButton: FC<Props> = ({ value, onClick, css }) => {
   return (
     <button
       onClick={onClick}
-      className={`h-1/2 w-full flex justify-center items-center + ${css}`}
+      className={`h-1/2 w-full flex justify-center items-center ${css}`}
     >
       <div className="w-32 h-10 border-gray-500 border-[1.5px] rounded-3xl flex justify-center items-center">
         <div>{value}</div>
