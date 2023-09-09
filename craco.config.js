c0777ed201aca371d880bf4cbaa0d5d2d91cbdd7
luckylooky2 <luckylooky2@naver.com>
2023-09-09 23:31:13 +0900

    feat : Add jest config to craco.config.json #10

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
+  // typescript에서 path alias를 사용하게 하는 webpack plugin 설정
   plugins: [
     {
       plugin: {
@@ -11,4 +12,17 @@ module.exports = {
       },
     },
   ],
+  // jest에서 path alias를 사용하기 위한 설정
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        "^\\@components/(.*)$": "<rootDir>/src/components/$1",
+        "^\\@contexts/(.*)$": "<rootDir>/src/contexts/$1",
+        "^\\@layouts/(.*)$": "<rootDir>/src/layouts/$1",
+        "^\\@pages/(.*)$": "<rootDir>/src/pages/$1",
+        "^\\@styles/(.*)$": "<rootDir>/src/styles/$1",
+        "^\\@utils/(.*)$": "<rootDir>/src/utils/$1",
+      },
+    },
+  },
 };
