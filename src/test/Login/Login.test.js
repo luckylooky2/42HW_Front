854b9ce1e6f89db34a04ca769c70733461e13c90
luckylooky2 <luckylooky2@naver.com>
2023-09-10 01:04:01 +0900

    rename : test file tsx to js #11

--- src/test/Login/Login.test.js
+++ src/test/Login/Login.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Login from "@pages/Login";
 
 describe("Login Component", () => {
-  test("renders without errors", () => {
+  it("renders without errors", () => {
     render(<Login />);
 
     // 특정 요소를 검사하여 렌더링이 정상적으로 되었는지 확인
