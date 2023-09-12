38f78238b47cc3fb87a853677f75d77bee9f0973
luckylooky2 <luckylooky2@naver.com>
2023-09-12 22:43:28 +0900

    feat : open topic modal animation #5

--- src/components/Call/TopicSelect.tsx
+++ src/components/Call/TopicSelect.tsx
@@ -1,6 +1,10 @@
+import { useContext, useEffect, useState } from "react";
 import TopicButton from "./TopicButton";
+import { SocketContext } from "@contexts/SocketProvider";
 
 const TopicSelect = () => {
+  const { socket } = useContext(SocketContext);
+  const [isOpen, setIsOpen] = useState(false);
   const topicList: string[] = [
     "shopping",
     "business",
@@ -12,15 +16,30 @@ const TopicSelect = () => {
     "hobbies",
     "42",
   ];
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setIsOpen(true);
+    }, 100);
+
+    return () => {
+      setIsOpen(false);
+      clearTimeout(id);
+    };
+  }, []);
+
   return (
-    <div className="h-[90%] max-w-[300px] grid grid-cols-3 mx-auto">
+    <div
+      className="h-[90%] max-w-[300px] grid grid-cols-3 mx-auto"
+      style={{ transform: `scale(${isOpen ? 1 : 0})`, transition: ".5s" }}
+    >
       {topicList.map((v, i) => (
         <TopicButton
           key={`topic-button-${v}`}
           text={v}
           img="game.svg"
           onClick={() => {
-            console.log(123);
+            socket?.emit("chooseTopic", {});
           }}
         />
       ))}
