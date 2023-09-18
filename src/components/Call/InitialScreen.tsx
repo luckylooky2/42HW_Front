2a86df3e943397b1e5596b3ae95471d8f4fca6a5
luckylooky2 <luckylooky2@naver.com>
2023-09-18 16:06:15 +0900

    design: remove props #5

--- src/components/Call/InitialScreen.tsx
+++ src/components/Call/InitialScreen.tsx
@@ -1,13 +1,18 @@
-import React, { FC } from "react";
+import React from "react";
 
-interface Props {
-  opponentStatus: boolean;
-}
-
-const InitialScreen: FC<Props> = ({ opponentStatus }) => {
+const InitialScreen = () => {
   return (
-    <div className="flex justify-center">
-      <div>{!opponentStatus && <div>상대방이 연결을 종료하였습니다.</div>}</div>
+    <div className="flex justify-center w-full h-full p-5">
+      <div className="bg-gray-100 w-full h-full max-w-[350px] mx-auto rounded-lg overflow-auto">
+        <div className="p-5">
+          <div>
+            <p>
+              <div>Start talking!</div>
+              <div>What is your name?</div>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
