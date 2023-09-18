99559589200ca65d9a74308a558c4f7bfc9b5463
luckylooky2 <luckylooky2@naver.com>
2023-09-18 16:03:44 +0900

    design: each topic button vertical align #5

--- src/components/Call/TopicButton.tsx
+++ src/components/Call/TopicButton.tsx
@@ -8,14 +8,16 @@ interface Props {
 
 const TopicButton: FC<Props> = ({ onClick, text, img }) => {
   return (
-    <div className="mx-auto">
-      <button
-        className="w-16 h-16 rounded-full flex justify-center items-center bg-orange-100 hover:bg-orange-300"
-        onClick={onClick}
-      >
-        <img className="w-8 h-8" src={img} alt="hang-up" />
-      </button>
-      {text && <div className="text-center">{text}</div>}
+    <div className="mx-auto flex items-center">
+      <div className="">
+        <button
+          className="w-16 h-16 rounded-full flex justify-center items-center bg-orange-100 hover:bg-orange-300"
+          onClick={onClick}
+        >
+          <img className="w-8 h-8" src={img} alt="hang-up" />
+        </button>
+        {text && <div className="text-center">{text}</div>}
+      </div>
     </div>
   );
 };
