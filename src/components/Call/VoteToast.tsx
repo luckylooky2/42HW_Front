e2491ea4f8e081e306893b4a2bd420ca4ab6d7e4
luckylooky2 <luckylooky2@naver.com>
2023-09-21 18:20:39 +0900

    feat: vote UI with buttons on requestees toast #22

--- src/components/Call/VoteToast.tsx
+++ src/components/Call/VoteToast.tsx
@@ -1,29 +1,45 @@
-import { FC, useState, useCallback, useEffect, useContext } from "react";
+import {
+  FC,
+  useState,
+  useCallback,
+  useEffect,
+  useContext,
+  useRef,
+} from "react";
 import { SocketContext } from "@contexts/SocketProvider";
 import { StreamContext } from "@contexts/StreamProvider";
+import { VOTE_SELECT } from "@utils/constant";
+import { CallInfo } from "@typings/Call";
 
 interface Props {
   contentsName: string;
   requester: string;
+  callType: CallInfo;
 }
 
-const VoteToast: FC<Props> = ({ contentsName, requester }) => {
+const VoteToast: FC<Props> = ({ contentsName, requester, callType }) => {
   const { socket } = useContext(SocketContext);
   const { streamInfo } = useContext(StreamContext);
-  const [voteStatus, setVoteStatus] = useState<string[]>(["✅", "➖"]);
-  const [isVoted, setIsVoted] = useState(false);
+  // TODO : custom hook
+  const [voteStatus, setVoteStatus] = useState<string[]>(
+    [VOTE_SELECT.YES].concat(
+      new Array(callType.OPPONENT_NUM).fill(VOTE_SELECT.ONGOING)
+    )
+  );
+  const indexRef = useRef<number>(1);
+  const [isVoted, setIsVoted] = useState<boolean | null>(null);
 
   const onSomeoneAccept = useCallback(() => {
     const copy = voteStatus.map((v) => v);
-    copy.pop();
-    copy.push("✅");
+    copy[indexRef.current] = VOTE_SELECT.YES;
+    indexRef.current++;
     setVoteStatus(copy);
   }, [voteStatus]);
 
   const onSomeoneReject = useCallback(() => {
     const copy = voteStatus.map((v) => v);
-    copy.pop();
-    copy.push("❌");
+    copy[indexRef.current] = VOTE_SELECT.NO;
+    indexRef.current++;
     setVoteStatus(copy);
   }, [voteStatus]);
 
@@ -40,7 +56,7 @@ const VoteToast: FC<Props> = ({ contentsName, requester }) => {
       roomName: streamInfo.roomName,
       contents: contentsName,
     });
-    setIsVoted(true);
+    setIsVoted(false);
   }, [isVoted]);
 
   useEffect(() => {
@@ -53,23 +69,46 @@ const VoteToast: FC<Props> = ({ contentsName, requester }) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <div>{`${requester} 님이 ${contentsName}을 요청하셨습니다. 수락하시겠습니까?`}</div>
-        <div>
-          {voteStatus.map((v, i) => (
-            <span key={`voteToast + ${v} + ${i}`}>{v}</span>
-          ))}
-        </div>
+    <div className="flex flex-col justify-evenly">
+      <div className="my-1">{`[${requester}] TOPIC-${contentsName} 요청`}</div>
+      <div
+        className={`grid grid-cols-${callType.TOTAL_NUM} w-full my-1 mx-auto`}
+      >
+        {voteStatus.map((v, i) => (
+          <div className="p-[2px]">
+            <div
+              className={`h-[20px] bg-${
+                v === VOTE_SELECT.ONGOING
+                  ? "gray"
+                  : v === VOTE_SELECT.YES
+                  ? "green"
+                  : "red"
+              }-500`}
+              key={`voteToast + ${v} + ${i}`}
+            />
+          </div>
+        ))}
       </div>
-      {!isVoted ? (
-        <>
-          <button onClick={voteAccept}>✅</button>
-          <button onClick={voteReject}>❌</button>
-        </>
-      ) : (
-        ""
-      )}
+      <div className="flex justify-evenly my-1">
+        {isVoted === null && (
+          <>
+            <button
+              className="w-[40%] h-[30px] rounded-md bg-blue-600"
+              onClick={voteAccept}
+            >
+              찬성
+            </button>
+            <button
+              className="w-[40%] h-[30px] rounded-md bg-gray-400"
+              onClick={voteReject}
+            >
+              반대
+            </button>
+          </>
+        )}
+        {isVoted === true && <div>찬성하셨습니다.</div>}
+        {isVoted === false && <div>반대하셨습니다.</div>}
+      </div>
     </div>
   );
 };
