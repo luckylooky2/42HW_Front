b36d2bbba1e7dedf5809cc22b9dd8b61a2d11f7c
luckylooky2 <luckylooky2@naver.com>
2023-09-22 19:53:54 +0900

    design: basic button with animation

--- src/utils/BasicButton.tsx
+++ src/utils/BasicButton.tsx
@@ -10,7 +10,9 @@ const BasicButton: FC<Props> = ({ onClick, text, disabled }) => {
   return (
     <button
       className={`w-40 h-8 my-2 block rounded-md ${
-        disabled ? "bg-gray-100" : "bg-orange-100 hover:bg-orange-200"
+        disabled
+          ? "bg-gray-100"
+          : "bg-orange-100 hover:bg-orange-200 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-200"
       }`}
       onClick={onClick}
       disabled={disabled}
