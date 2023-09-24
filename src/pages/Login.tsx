ee15cf685d0f39e09aab1c7c9834cabcae609fbf
luckylooky2 <luckylooky2@naver.com>
2023-09-24 11:10:03 +0900

    feat: show loading first in login component for UX #28

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,29 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, FC, Dispatch } from "react";
 import { API_URL } from "@utils/constant";
+import { useNavigate } from "react-router";
+import Loading from "@utils/Loading";
+import { getCookie } from "@utils/getCookie";
 
-const Login = () => {
+interface Props {
+  login: boolean;
+  setLogin: Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Login: FC<Props> = ({ login, setLogin }) => {
+  const navigate = useNavigate();
   const clickLogin = useCallback(() => {
     window.location.href = `${API_URL}/auth/login`;
   }, []);
 
-  return (
-    <div className="flex flex-col h-full justify-center	">
+  useEffect(() => {
+    const cookie = getCookie("at");
+    const isLogin = localStorage.getItem("isLogin");
+    if (isLogin === "true" && cookie) navigate("/main");
+    else setLogin(true);
+  }, []);
+
+  return login ? (
+    <div className="flex flex-col h-full justify-center	relative">
       <div className="px-10 h-1/4">
         <div className="text-4xl my-4">Hello World</div>
         <div>Have fun playing games </div>
@@ -16,7 +32,7 @@ const Login = () => {
       <div className="pt-8 h-1/3">
         <img className="mx-auto" src="login-image.svg" alt="login" />
       </div>
-      <div className="flex justify-center items-center	 h-1/6">
+      <div className="flex justify-center items-center h-1/6">
         <button
           className="w-80 h-10 text-black bg-orange-100 rounded-lg text-center hover:bg-orange-200"
           onClick={clickLogin}
@@ -24,17 +40,19 @@ const Login = () => {
           Sign in with 42
         </button>
       </div>
+      <footer className="h-[5%] text-center text-gray-500 flex justify-center items-center absolute bottom-0 w-full">
+        <div className="py-auto">
+          <small className="italic">Copyright &copy; 2023 Mozila</small>
+          <small> | </small>
+          <small>문의하기</small>
+        </div>
+      </footer>
     </div>
+  ) : (
+    <Loading />
   );
 };
 
 export default Login;
 
 // TODO : 로그인 페이지에 추가
-/* <footer className="h-[5%] text-center text-gray-500 flex justify-center items-center">
-  <div className="py-auto">
-    <small className="italic">Copyright &copy; 2023 Mozila</small>
-    <small> | </small>
-    <small>문의하기</small>
-  </div>
-</footer>; */
