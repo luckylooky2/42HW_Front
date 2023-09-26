74359a84c6c3cc99599888d3d1c6935c6125e5ac
luckylooky2 <luckylooky2@naver.com>
2023-09-26 22:12:26 +0900

    feat: can't access 404 page when not login

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,14 +1,21 @@
 import BasicButton from "@utils/BasicButton";
-import { useCallback } from "react";
+import { FC, useCallback } from "react";
 import { useNavigate } from "react-router";
+import Loading from "@utils/Loading";
 
-const NotFound = () => {
+interface Props {
+  isLoading: boolean;
+}
+
+const NotFound: FC<Props> = ({ isLoading }) => {
   const navigate = useNavigate();
   const onBackToMain = useCallback(() => {
     navigate("/main");
   }, []);
 
-  return (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <div className="w-full h-full">
       <div>Page Not Found!</div>
       <BasicButton text="메인으로 돌아가기" onClick={onBackToMain} />
