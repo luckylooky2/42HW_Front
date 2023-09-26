52687cbe45d271cb658afbd299d02f920fa9a5d5
luckylooky2 <luckylooky2@naver.com>
2023-09-26 21:52:26 +0900

    fix: redirect to / when login token not exist in useLayoutEffect #32

--- src/utils/MainRouter.tsx
+++ src/utils/MainRouter.tsx
@@ -1,5 +1,5 @@
-import React, { Dispatch, FC, useLayoutEffect, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router";
+import React, { useLayoutEffect, useState } from "react";
+import { Routes, Route } from "react-router";
 import Main from "@pages/Main";
 import Waiting from "@pages/Waiting";
 import Call from "@pages/Call";
@@ -7,28 +7,17 @@ import Setting from "@pages/Setting";
 import NotFound from "@pages/NotFound";
 import { getCookie } from "@utils/getCookie";
 
-interface Props {
-  setLogin: Dispatch<React.SetStateAction<boolean>>;
-}
-
-const MainRouter: FC<Props> = ({ setLogin }) => {
-  const navigate = useNavigate();
+const MainRouter = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
   useLayoutEffect(() => {
     const cookie = getCookie("login");
     const isLogin = localStorage.getItem("isLogin");
     if (!(isLogin === "true" && cookie)) {
       alert("로그인 정보가 유효하지 않습니다. 다시 로그인 해주세요.");
-    }
-  }, []);
-
-  useEffect(() => {
-    const cookie = getCookie("login");
-    const isLogin = localStorage.getItem("isLogin");
-    if (!(isLogin === "true" && cookie)) {
-      setLogin(true);
-      navigate("/");
-    }
+      localStorage.removeItem("isLogin");
+      window.location.href = "/";
+    } else setIsLoading(false);
   }, []);
 
   return (
@@ -37,7 +26,7 @@ const MainRouter: FC<Props> = ({ setLogin }) => {
       <Route path="/setting" Component={Setting} />
       <Route path="/waiting" Component={Waiting} />
       <Route path="/call" Component={Call} />
-      <Route path="*" Component={NotFound} />
+      <Route path="*" element={<NotFound isLoading={isLoading} />} />
     </Routes>
   );
 };
