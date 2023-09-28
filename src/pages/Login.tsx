9b644bba4c16579cfe70b8000c6dab959870dd49
luckylooky2 <luckylooky2@naver.com>
2023-09-28 22:33:11 +0900

    design: pulse loader when login button clicked #24

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,12 +4,16 @@ import { useNavigate } from "react-router";
 import Loading from "@utils/Loading";
 import { getCookie } from "@utils/manageCookie";
 import { useTranslation } from "react-i18next";
+import { PulseLoader } from "react-spinners";
 
 const Login = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const [login, setLogin] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const clickLogin = useCallback(() => {
+    setLoading(true);
     window.location.href = `${API_URL}/auth/login`;
   }, []);
 
@@ -35,7 +39,16 @@ const Login = () => {
           className="w-80 h-10 text-black bg-orange-100 rounded-lg text-center hover:bg-orange-200"
           onClick={clickLogin}
         >
-          Sign in with 42
+          {loading ? (
+            <PulseLoader
+              color={"#808080"}
+              size={10}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          ) : (
+            "Sign in with 42"
+          )}
         </button>
       </div>
       <footer className="h-[5%] text-center text-gray-500 flex justify-center items-center absolute bottom-0 w-full">
