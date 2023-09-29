0cc650cb1a09158aa1bf2959ed26fe0f8acb0d62
luckylooky2 <luckylooky2@naver.com>
2023-09-29 23:52:06 +0900

    fix: move opponent status next to opponent nickname #16

--- src/components/Call/Timer.tsx
+++ src/components/Call/Timer.tsx
@@ -1,12 +1,8 @@
-import { FC, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 import { timeConverter } from "@utils/timeConverter";
 
-interface Props {
-  opponentStatus: boolean;
-}
-
-const Timer: FC<Props> = ({ opponentStatus }) => {
+const Timer = () => {
   const [startTime] = useState<Date>(new Date());
   const [diff, setDiff] = useState<string>("00:00");
 
@@ -18,18 +14,12 @@ const Timer: FC<Props> = ({ opponentStatus }) => {
       setDiff(timeConverter(diffSeconds, "colon"));
     }, 500);
 
-    if (opponentStatus === false) clearInterval(id);
-
     return () => {
       clearInterval(id);
     };
-  }, [opponentStatus]);
+  }, []);
 
-  return (
-    <div className="text-gray-400 text-xl text-center">
-      {opponentStatus ? "🟢 " : "🔴 "} {diff}
-    </div>
-  );
+  return <div className="text-gray-400 text-xl text-center">{diff}</div>;
 };
 
 export default Timer;
