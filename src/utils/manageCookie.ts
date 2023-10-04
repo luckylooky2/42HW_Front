7af59dee68eb7f1e3465c1045c5bf5c80c77f471
luckylooky2 <luckylooky2@naver.com>
2023-10-04 14:07:31 +0900

    fix: add domain string when delete cookie #33

--- src/utils/manageCookie.ts
+++ src/utils/manageCookie.ts
@@ -3,6 +3,8 @@ export function getCookie(name: string) {
   return value ? value[2] : null;
 }
 
+// Domain 설정을 해주지 않으면 안 됨!
 export function deleteCookie(name: string) {
-  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+  document.cookie =
+    name + "=; Domain=helloworld42.com; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
