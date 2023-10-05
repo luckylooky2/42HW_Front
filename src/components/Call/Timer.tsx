dfdfd25f8e0948b492588494843e797bb8810e67
luckylooky2 <luckylooky2@naver.com>
2023-10-05 19:28:07 +0900

    feat: Timer end when only one person left #16

--- src/components/Call/Timer.tsx
+++ src/components/Call/Timer.tsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import dayjs from "dayjs";
 import { timeConverter } from "@utils/timeConverter";
+import { CallContext } from "@contexts/CallProvider";
 
 const Timer = () => {
+  const { callInfo } = useContext(CallContext);
   const [startTime] = useState<Date>(new Date());
   const [diff, setDiff] = useState<string>("00:00");
+  const intervalId = useRef<any>(0);
 
   useEffect(() => {
     const id = setInterval(() => {
@@ -13,11 +16,16 @@ const Timer = () => {
       const diffSeconds = now.diff(start, "second");
       setDiff(timeConverter(diffSeconds, "colon"));
     }, 500);
+    intervalId.current = id;
 
     return () => {
       clearInterval(id);
     };
   }, []);
+
+  useEffect(() => {
+    if (callInfo.currNum === 1) clearInterval(intervalId.current);
+  }, [callInfo]);
 
   return <div className="text-gray-400 text-xl text-center">{diff}</div>;
 };
