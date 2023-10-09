6dba3d3aa4475042fd883906816a2ddf82b4e39e
luckylooky2 <luckylooky2@naver.com>
2023-10-09 13:31:13 +0900

    feat: add Setting, Waiting page locale #39

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,19 +1,31 @@
 import MainKr from "./ko/Main.kr.json";
 import LoginKr from "./ko/Login.kr.json";
 import ProfileKr from "./ko/Profile.kr.json";
+import SettingKr from "./ko/Setting.kr.json";
+import WaitingKr from "./ko/Waiting.kr.json";
+import CallKr from "./ko/Call.kr.json";
 
 import MainEn from "./en/Main.en.json";
 import LoginEn from "./en/Login.en.json";
-import ProfileEN from "./en/Profile.en.json";
+import ProfileEn from "./en/Profile.en.json";
+import SettingEn from "./en/Setting.en.json";
+import WaitingEn from "./en/Waiting.en.json";
+import CallEn from "./en/Call.en.json";
 
 export const translationKr = {
   login: LoginKr,
   main: MainKr,
   profile: ProfileKr,
+  setting: SettingKr,
+  waiting: WaitingKr,
+  call: CallKr,
 };
 
 export const translationEn = {
   login: LoginEn,
   main: MainEn,
-  profile: ProfileEN,
+  profile: ProfileEn,
+  setting: SettingEn,
+  waiting: WaitingEn,
+  call: CallEn,
 };
