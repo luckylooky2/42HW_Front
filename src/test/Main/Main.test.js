6a5c95ef35bceaef49ebb098eb2633185d5111df
luckylooky2 <luckylooky2@naver.com>
2023-12-13 22:42:23 +0900

    feat: [TEST-Main] initialize useContext, show Loading component #47

--- src/test/Main/Main.test.js
+++ src/test/Main/Main.test.js
@@ -50,7 +50,7 @@ afterEach(() => {
 });
 
 describe("Main Component", () => {
-  it("렌더링 및 번역 확인", () => {
+  it("useContext 초기화 및 초기 Loading 컴포넌트 렌더링 확인", () => {
     useNavigate.mockReturnValue(mockNavigate);
     useTranslation.mockReturnValue(mockT);
     axios.get.mockReturnValue(getUserValue);
@@ -63,11 +63,15 @@ describe("Main Component", () => {
 
     render(<Main />);
 
-    expect(spy).toHaveBeenCalled();
-    // const greeting1Text = screen.getByText(translationKo.login.greeting1);
+    // TODO : dot notaion으로 state 값으로 확인하기
+    expect(spy).nthReturnedWith(1, mockAuthContext);
+    expect(spy).nthReturnedWith(2, mockSocketContext);
+    expect(spy).nthReturnedWith(3, mockCallContext);
+    const spinnerElement = screen.getByTestId("loader");
+    expect(spinnerElement).toBeInTheDocument();
   });
 
-  //   it("프로필 페이지 이동", () => {});
+  // it("프로필 페이지 이동", () => {});
   //   it("1:1 채팅 참여", () => {});
   //   it("그룹 채팅 참여", () => {});
   //   it("소켓 연결 확인", () => {});
