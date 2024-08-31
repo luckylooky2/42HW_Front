bfb045badc0bada3da3e2c50e2611c616ef41534
luckylooky2 <luckylooky2@naver.com>
2024-08-31 23:08:57 +0900

    Env: node.js process.nextTick 부재에 따른 폴리필 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,29 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   server: { port: 3000 },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@utils": path.resolve(__dirname, "./src/utils"),
@@ -15,6 +33,13 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@styles": path.resolve(__dirname, "./src/styles"),
       i18n: path.resolve(__dirname, "./src/i18n.js"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      // Enable rollup polyfills plugin
+      // used during production bundling
+      plugins: [nodePolyfills()],
     },
   },
 });
