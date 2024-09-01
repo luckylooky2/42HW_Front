cba6d99c776a97026318ee77fd9aafd29d0835ba
luckylooky2 <luckylooky2@naver.com>
2024-09-01 23:59:59 +0900

    Fix: Header 레이아웃 children prop 추가

--- src/utils/Header.tsx
+++ src/utils/Header.tsx
@@ -1,24 +1,28 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
 interface Props {
-  onClick: () => void;
+  onClick?: () => void;
   title: string;
+  children: ReactNode;
 }
 
-const Header: FC<Props> = ({ onClick, title }) => {
+const Header: FC<Props> = ({ onClick, title, children }) => {
   return (
-    <div
-      className={`h-[10%] flex items-center justify-between bg-orange-100 w-full`}
-    >
-      <button
-        className="w-[10%] h-full m-2 flex items-center justify-center"
-        onClick={onClick}
+    <>
+      <div
+        className={`h-[10%] flex items-center justify-between bg-orange-100 w-full`}
       >
-        <img width="25" height="25" src="arrow.png" alt="profile_back" />
-      </button>
-      <b className="text-lg">{title}</b>
-      <div className="w-[10%] h-full m-2 flex items-center justify-center" />
-    </div>
+        <button
+          className="w-[10%] h-full m-2 flex items-center justify-center"
+          onClick={onClick}
+        >
+          <img width="25" height="25" src="arrow.png" alt="profile_back" />
+        </button>
+        <b className="text-lg">{title}</b>
+        <div className="w-[10%] h-full m-2 flex items-center justify-center" />
+      </div>
+      <div className="h-[90%] w-full p-4">{children}</div>
+    </>
   );
 };
 
