8d4fd1757a0ee94573523491167bb2cbb3ebe4c9
luckylooky2 <luckylooky2@naver.com>
2024-09-03 17:27:00 +0900

    Env: vite에 eslint 플러그인 및 import.meta.env typescript 처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: { port: 3000 },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}',
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
@@ -32,6 +43,7 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@components": path.resolve(__dirname, "./src/components"),
       "@styles": path.resolve(__dirname, "./src/styles"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
       i18n: path.resolve(__dirname, "./src/i18n.js"),
     },
   },
