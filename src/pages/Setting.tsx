41b352ccff035498dea3ce2f7df1440ef5f4e45b
luckylooky2 <luckylooky2@naver.com>
2024-09-03 16:51:15 +0900

    Fix: 마이크 세팅 페이지에 마이크 입력 목록, 마이크 설정 커스텀 훅 적용

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -1,185 +1,102 @@
-import {
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-  MutableRefObject,
-} from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router";
 import { SocketContext } from "@contexts/SocketProvider";
 import { AuthContext } from "@contexts/AuthProvider";
-import { CallContext, CallActionType } from "@contexts/CallProvider";
 import { toast } from "react-toastify";
 import BasicButton from "@utils/BasicButton";
 import MicrophoneSoundChecker from "@utils/MicrophoneSoundChecker";
-import { PAGE, SINGLE_CALL, TRANSLATION } from "@utils/constant";
+import { PAGE, TRANSLATION } from "@utils/constant";
 import Loading from "@utils/Loading";
 import { useTranslation } from "react-i18next";
 import Header from "@utils/Header";
+import { useStream } from "@hooks/useStream";
+import { useAudioInput } from "@hooks/useAudioInput";
+import { useRoomType } from "@hooks/useRoomType";
 
 const Setting = () => {
   const navigate = useNavigate();
   const { t } = useTranslation(TRANSLATION);
-  const [isDone, setIsDone] = useState(false);
   const { myInfo } = useContext(AuthContext);
   const { socket } = useContext(SocketContext);
-  const { callInfo, dispatch } = useContext(CallContext);
-  const streamRef = useRef<MediaStream>(new MediaStream());
-  const [audioInputList, setAudioInputList] = useState<MediaDeviceInfo[]>([]);
-  const [currInputDeviceId, setCurrInputDeviceId] =
-    useState("마이크를 선택해 주세요.");
-
-  console.log(callInfo.deviceId, currInputDeviceId);
+  const [roomType] = useRoomType();
+  const { deviceId, stream, connectStream, disconnectStream } = useStream();
+  const { audioInputs, selected, setSelected, refresh, findDeviceIdIndex } =
+    useAudioInput();
 
   useEffect(() => {
     // 잘못된 접근했을 때
     if (myInfo === null || socket === null) {
       navigate("/main");
     }
-  }, []);
+  }, [myInfo, socket]);
 
-  const findTargetIndex = (
-    arr: MediaDeviceInfo[],
-    target: string | null
-  ): number => {
-    let targetIndex = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-      const { deviceId } = arr[i];
-      if (target === deviceId) {
-        targetIndex = i;
-      }
+  // 이전 선택 그대로 유지
+  useEffect(() => {
+    if (audioInputs.length) {
+      const index = findDeviceIdIndex(deviceId);
+      getMicrophone(audioInputs[index].deviceId);
     }
-    return targetIndex;
+  }, [audioInputs]);
+
+  const getMicrophone = async (deviceId: string) => {
+    if (myInfo == null || socket === null) {
+      return;
+    }
+
+    disconnectStream();
+    await connectStream(deviceId);
+    setSelected(deviceId);
   };
 
-  // 크롬에서는 원래 거부되었을 경우 : denied
-  // 사파리에서는 : denied -> prompt로 바뀜
-  useEffect(() => {
-    getDeviceList();
-  }, [socket]);
-
-  useEffect(() => {
-    if (audioInputList.length) {
-      const targetIndex = findTargetIndex(audioInputList, callInfo.deviceId);
-      setCurrInputDeviceId(audioInputList[targetIndex].deviceId);
-      getUserMedia(audioInputList[targetIndex].deviceId);
-    }
-  }, [audioInputList]);
-
-  const getUserMedia = useCallback(
-    async (deviceId: string | undefined = undefined) => {
-      if (myInfo == null || socket === null) return;
-
-      let newStream;
-      try {
-        newStream = await navigator.mediaDevices.getUserMedia({
-          video: false,
-          audio: { deviceId: { exact: deviceId } },
-        });
-
-        dispatch({
-          type: CallActionType.SET_STREAM,
-          payload: { stream: newStream, deviceId: deviceId },
-        });
-        setIsDone(true);
-
-        streamRef.current.getAudioTracks().forEach((track) => {
-          track.stop();
-        });
-
-        streamRef.current = newStream;
-      } catch (e) {
-        // granted 인데 DOMException: Permission denied by system(Arc), NotAllowedError: The request is not allowed by the user agent or the platform in the current context, possibly because the user denied permission.(Safari)이 뜨는 경우도 있음
-        console.log("getUserMedia", e);
-        // 크롬에서는 괜찮은데, 사파리에서는 granted라도 prompt 창이 떠야 연결이 됨
-        // granted 가지고만 판단하면 안 됨
-        // sarafi에서는 몇 번 하면, 세션에 저장되어 프롬프트가 뜨지 않음
-        // INFO : 설정을 변경한 후 꼭 새로고침을 해주세요
-        // INFO : safari에서 계속해서 마이크가 켜지지 않는 경우에는 창을 껐다 켜주세요
-      }
-    },
-    [isDone, audioInputList]
-  );
-
-  const getDeviceList = useCallback(async () => {
-    try {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const excludeDefault = devices.filter((v) => v.deviceId !== "default");
-      const inputs = excludeDefault.filter((v) => v.kind === "audioinput");
-
-      setAudioInputList(inputs);
-    } catch (e) {
-      console.log("enumerateDevices", e);
-    }
-  }, [audioInputList]);
-
   const goToMain = useCallback(() => {
-    if (streamRef.current) {
-      streamRef.current.getTracks().forEach((track) => {
-        track.stop();
-      });
-    }
-    dispatch({
-      type: CallActionType.DEL_ALL,
-    });
-
     navigate("/main");
   }, []);
 
   const goToWaiting = useCallback(() => {
-    if (!streamRef.current.active) {
+    if (!(stream && stream.active)) {
       toast.error("마이크가 연결되지 않았습니다.");
       return;
     }
     navigate("/waiting");
-  }, []);
+  }, [stream]);
+
+  const handleInput = ({ target }) => {
+    const targetDeviceId = target.value;
+    getMicrophone(targetDeviceId);
+  };
 
   return socket === null ? (
     <Loading />
   ) : (
     <Header
       onClick={goToMain}
-      title={`Microphone Setting: ${t(
-        `${PAGE.MAIN}.` +
-          (callInfo.roomType === SINGLE_CALL.TYPE ? "singleCall" : "groupCall")
-      )}`}
+      title={`Microphone Setting: ${t(`${PAGE.MAIN}.${roomType}Call`)}`}
     >
       <div className="flex flex-col w-full h-full justify-around items-center">
         <section className="flex flex-col justify-center items-center gap-10">
           <div className="w-[350px] h-[300px] flex flex-col justify-around items-center gap-2 rounded-lg bg-gray-100">
             <div className="flex justify-around items-center gap-5 h-[200px]">
-              <MicrophoneSoundChecker isDone={isDone} icon={true} />
+              <MicrophoneSoundChecker stream={!!stream} icon={true} />
             </div>
             <div className="flex w-[90%] justify-center gap-3">
               <div>
                 <label>Mic: </label>
                 <select
                   className="w-[200px] rounded-md"
-                  onInput={({ target }) => {
-                    const deviceId = target.value;
-                    const targetIndex = findTargetIndex(
-                      audioInputList,
-                      deviceId
-                    );
-                    console.log(targetIndex);
-                    setCurrInputDeviceId(audioInputList[targetIndex].deviceId);
-                    getUserMedia(deviceId);
-                  }}
-                  value={currInputDeviceId}
+                  onInput={handleInput}
+                  value={selected}
                 >
-                  {audioInputList.length === 0 && (
-                    <option>마이크를 연결해주세요</option>
+                  {audioInputs.length === 0 && (
+                    <option>마이크를 연결해주세요.</option>
                   )}
-                  {audioInputList.map((audio) => (
+                  {audioInputs.map((audio) => (
                     <option key={audio.label} value={audio.deviceId}>
                       {audio.label}
                     </option>
                   ))}
                 </select>
               </div>
-              <button onClick={getDeviceList}>
+              <button onClick={refresh}>
                 <img
                   width="15"
                   height="15"
