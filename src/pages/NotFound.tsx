ff1925560c008b86ff85601c4341d096bd6015d8
luckylooky2 <luckylooky2@naver.com>
2024-09-11 22:59:06 +0900

    Fix: MainRouter isLoading 상태값 삭제

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,21 +1,14 @@
 import BasicButton from "@utils/BasicButton";
-import Loading from "@utils/Loading";
 import { FC, useCallback } from "react";
 import { useNavigate } from "react-router";
 
-interface Props {
-  isLoading: boolean;
-}
-
-const NotFound: FC<Props> = ({ isLoading }) => {
+const NotFound: FC = () => {
   const navigate = useNavigate();
   const onBackToMain = useCallback(() => {
     navigate("/main");
   }, []);
 
-  return isLoading ? (
-    <Loading />
-  ) : (
+  return (
     <div className="w-full h-full">
       <div>Page Not Found!</div>
       <BasicButton text="메인으로 돌아가기" onClick={onBackToMain} />
--- src/utils/MainRouter.tsx
+++ src/utils/MainRouter.tsx
@@ -4,12 +4,9 @@ import NotFound from "@pages/NotFound";
 import Profile from "@pages/Profile";
 import Setting from "@pages/Setting";
 import Waiting from "@pages/Waiting";
-import React, { useState } from "react";
 import { Routes, Route } from "react-router";
 
 const MainRouter = () => {
-  const [isLoading] = useState(true);
-
   return (
     <Routes>
       <Route path="/main" Component={Main} />
@@ -17,7 +14,7 @@ const MainRouter = () => {
       <Route path="/setting" Component={Setting} />
       <Route path="/waiting" Component={Waiting} />
       <Route path="/call" Component={Call} />
-      <Route path="*" element={<NotFound isLoading={isLoading} />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
