89d6eba28d60b8933fedd5a17c0ca5d91d68229b
luckylooky2 <luckylooky2@naver.com>
2024-09-13 10:04:32 +0900

    Fix: Router에서 Waiting 페이지 제거 #49

--- src/utils/MainRouter.tsx
+++ src/utils/MainRouter.tsx
@@ -3,7 +3,6 @@ import Main from "@pages/Main";
 import NotFound from "@pages/NotFound";
 import Profile from "@pages/Profile";
 import Setting from "@pages/Setting";
-import Waiting from "@pages/Waiting";
 import { Routes, Route } from "react-router";
 
 const MainRouter = () => {
@@ -12,7 +11,6 @@ const MainRouter = () => {
       <Route path="/main" Component={Main} />
       <Route path="/profile" Component={Profile} />
       <Route path="/setting" Component={Setting} />
-      <Route path="/waiting" Component={Waiting} />
       <Route path="/call" Component={Call} />
       <Route path="*" element={<NotFound />} />
     </Routes>
